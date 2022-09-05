4c560f834e3055d54df863f37c42c1f3df55b47b
chimin <fantazier@hotmail.com>
2022-09-05 12:27:12 +0800

    [sd] Use annotations for Swagger info

--- springdoc-demo/src/main/java/c4compile/springdocdemo/SpringdocDemoApplication.java
+++ springdoc-demo/src/main/java/c4compile/springdocdemo/SpringdocDemoApplication.java
@@ -1,13 +1,19 @@
 package c4compile.springdocdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Calculator",
+                version = "1.0.0"))
 public class SpringdocDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringdocDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocDemoApplication.class, args);
+    }
 
 }
